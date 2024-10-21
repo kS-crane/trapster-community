220aaf0a831de605e2d8179e0b6f0133cde0b688
kScrane <kscrane@umass.edu>
2024-10-21 16:17:09 +0200

    Initial portscanner module

---
+++ trapster/data/portscan/remove_nftable_rules.sh
@@ -0,0 +1,8 @@
+nft delete chain inet filter syn_scan
+nft delete chain inet filter input
+nft delete set inet filter syn_ip
+nft delete set inet filter ip_to_ports
+nft delete set inet filter xmas_ip
+nft delete set inet filter null_ip
+nft delete set inet filter fin_ip
+nft delete set inet filter os_ip
